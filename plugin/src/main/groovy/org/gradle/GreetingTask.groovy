566b9ba1c81a03e69f8b6f10f5e74f3dbab90a20
hisayoshi suehiro <hisayoshi.suehiro@access-company.com>
2017-05-24 22:08:42 +0900

    add sample plugin

---
+++ plugin/src/main/groovy/org/gradle/GreetingTask.groovy
@@ -0,0 +1,22 @@
+package org.gradle
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class GreetingTask extends DefaultTask {
+    String greeting = 'hello from GreetingTask'
+
+    @TaskAction
+    def greet() {
+        println greeting
+        def env = System.getenv()
+        //Print all the environment variables.
+
+        env.each{
+            println it
+        }
+        // You can also access the specific variable, say 'username', as show below
+        String user= env['USERNAME']
+
+    }
+}
